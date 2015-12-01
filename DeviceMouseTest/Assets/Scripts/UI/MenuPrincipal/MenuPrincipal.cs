3cef6f6ebdd66cc5b58b43b19c9ad5ef78459ced
unfolange <unfolange@gmail.com>
2015-11-30 23:46:51 -0500

    Incluir scripts

---
+++ DeviceMouseTest/Assets/Scripts/UI/MenuPrincipal/MenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPrincipal : MonoBehaviour {
+
+    // Use this for initialization
+    public void Empezar () {
+        Application.LoadLevel(1);
+           }
+
+    // Update is called once per frame
+    public void NuevoJuego () {
+	
+	}
+    public void Opciones()
+    {
+
+    }
+    public void Salir()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+}
